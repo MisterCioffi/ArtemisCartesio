5f65835a2153665fcab54d9a92ec9422e24fc1db
Antonio Cirino <112080821+antocirino@users.noreply.github.com>
2024-12-21 18:28:00 +0100

    Update viste.sql

--- Viste/viste.sql
+++ Viste/viste.sql
@@ -1,1 +1,3 @@
-
+create view membri_missioni as
+select m.nome, m.cognome, mi.obiettivo as Obiettivo_Missione
+from (partecipazioni p join membri m on p.membri = m.id) join missioni mi on p.missioni = mi.id
