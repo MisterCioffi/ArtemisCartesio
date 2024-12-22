9e2c572e70fe91a2c57697579a4ec9283853c7c5
Alessandro Cioffi <alessandrocioffi007@gmail.com>
2024-12-22 13:21:41 +0100

    Update procedure.sql

--- Procedure/procedure.sql
+++ Procedure/procedure.sql
@@ -4,11 +4,13 @@ CREATE OR REPLACE PROCEDURE AssegnareSensoreAMissione(
     p_Missione_ID IN NUMBER
 ) AS
 BEGIN
-    INSERT INTO UTILIZZO_SENSORI (Sensori, Missioni)
+    INSERT INTO UTILIZZO_SENSORI (Sensore, Missione)
     VALUES (p_Sensore_ID, p_Missione_ID);
     
     DBMS_OUTPUT.PUT_LINE('Sensore ' || p_Sensore_ID || ' assegnato alla missione ' || p_Missione_ID);
 END;
+--FUNZIONA, ESEMPIO SOTTO:
+EXECUTE AssegnareSensoreAMissione(p_Sensore_ID => 11, p_Missione_ID => 1);
 
 -- INSERIRE UN MEMBRO NELLA TABELLA COINVOLGIMENTI RELATIVAMENTE AD UN INTERVENTO
 CREATE OR REPLACE PROCEDURE InserireMembroInCoinvolgimento(
@@ -17,7 +19,7 @@ CREATE OR REPLACE PROCEDURE InserireMembroInCoinvolgimento(
 ) AS
 BEGIN
     -- Inserisce il membro nell'intervento in corso
-    INSERT INTO COINVOLGIMENTI (Membri, Interventi)
+    INSERT INTO COINVOLGIMENTI (Membro, Intervento)
     VALUES (p_Membro_ID, p_Intervento_ID);
     
     DBMS_OUTPUT.PUT_LINE('Membro con ID ' || p_Membro_ID || ' aggiunto all''intervento con ID ' || p_Intervento_ID);
@@ -27,3 +29,5 @@ EXCEPTION
     WHEN OTHERS THEN
         DBMS_OUTPUT.PUT_LINE('Errore imprevisto: ' || SQLERRM);
 END InserireMembroInCoinvolgimento;
+--FUNZIONA, ESEMPIO SOTTO
+EXECUTE InserireMembroInCoinvolgimento(p_Membro_ID => 15, p_Intervento_ID => 10);
