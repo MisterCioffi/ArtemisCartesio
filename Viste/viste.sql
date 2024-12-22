5ccb77973303e20ea349fe1c4349e90bddb3608b
antocirino <cirinoant@gmail.com>
2024-12-22 15:40:33 +0100

    Aggiunte viste per la visualizzazione dei membri e dei robot assegnati alle missioni

--- Viste/viste.sql
+++ Viste/viste.sql
@@ -1,3 +1,26 @@
-create view membri_missioni as
-select m.nome, m.cognome, mi.obiettivo as Obiettivo_Missione, mi.stato as Stato_Missione
-from (partecipazioni p join membri m on p.membri = m.id) join missioni mi on p.missioni = mi.id
+-- View per la visualizzazione dei membri (nome, cognome) e delle missioni a cui partecipano (obiettivo, stato)
+CREATE VIEW MEMBRI_MISSIONI AS
+SELECT 
+    M.NOME, 
+    M.COGNOME, 
+    MI.OBIETTIVO AS OBIETTIVO_MISSIONE, 
+    MI.STATO AS STATO_MISSIONE
+FROM 
+    (PARTECIPAZIONI P 
+    JOIN MEMBRI M ON P.MEMBRI = M.ID) 
+    JOIN MISSIONI MI ON P.MISSIONI = MI.ID;
+
+
+-- View per la visualizzazione dei sensori (tipo, stato operativo) e delle missioni a cui sono stati assegnati (obiettivo, data inizio)
+CREATE VIEW ROBOT_MISSIONI AS
+SELECT 
+    R.ID AS ID_ROBOT, 
+    R.TIPO AS TIPO_ROBOT, 
+    M.ID AS ID_MISSIONE, 
+    M.OBIETTIVO AS OBIETTIVO_MISSIONE
+FROM 
+    (UTILIZZO_ROBOT UR 
+    JOIN ROBOT R ON UR.ROBOT = R.ID) 
+    JOIN MISSIONI M ON UR.MISSIONE = M.ID;
+
+
