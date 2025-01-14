f98a8f12ffd07395e715bbbb9418fc4a3d41ed2b
Antonio Cirino <112080821+antocirino@users.noreply.github.com>
2025-01-14 17:10:34 +0100

    Update indici.sql

--- Indici/indici.sql
+++ Indici/indici.sql
@@ -1,14 +1,14 @@
--- utile per filtrare missioni sullo stato
+-- Per filtrare missioni sullo stato
 CREATE INDEX idx_missioni_stato ON MISSIONI(Stato); 
 
--- utile per filtrare sul ruolo dei membri
+-- Per filtrare sul ruolo dei membri
 CREATE INDEX idx_membri_ruolo ON MEMBRI(Ruolo); 
 
--- utile per filtrare sul tipo di robot
+-- Per filtrare sul tipo di robot
 CREATE INDEX idx_robot_tipo ON ROBOT(Tipo); 
 
--- utile per filtrare sulla data del report
+-- Per filtrare sulla data del report
 CREATE INDEX idx_report_data ON REPORT(Data);
 
--- utile per filtrare sulla coppia sensore-data di una rilevazione
+-- Per filtrare sulla coppia sensore-data di una rilevazione
 CREATE INDEX idx_rilevazioni_sensori_data ON RILEVAZIONI(Sensore, Data);
